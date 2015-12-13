7107b82b373945711f10b5f88300e32062dde12f
IaraRibeiro <iara.prbr@gmail.com>
2015-12-13 12:07:18 -0300

    informações de diretor

--- formatData/get_awards.sh
+++ formatData/get_awards.sh
@@ -5,8 +5,8 @@ javac /home/iara/hackfest/graphollywood/formatData/readData.java
 while read p; do
   echo "executando para "$p
   curl http://www.imdb.com/name/nm$p/awards?ref_=nm_ql_2 > /tmp/$p".txt"
-  java readData /tmp/$p".txt" >> awards.csv
+  java readData /tmp/$p".txt" >> director_awards.csv
   rm -rf /tmp/$p".txt"
   echo "proximo ator."
 
-done </home/iara/hackfest/graphollywood/formatData/actors.txt
+done </home/iara/hackfest/graphollywood/formatData/directors.txt
