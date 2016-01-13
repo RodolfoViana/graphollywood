fcf6dbac3c33634be5cc45af8830e232202d2084
Antonio Marques Jr <antonioricardojr@gmail.com>
2016-01-13 15:13:32 -0300

    adding directors avatars into short bio bellow the graph

---
+++ data/generate_json_directors_pic.py
@@ -0,0 +1,33 @@
+import re
+import json
+
+data = open('information_directors.json', 'r')
+
+data_json = json.load(data)
+
+directors = data_json["directors"][1:]
+
+for d in directors:
+	
+	director_name = d['FIELD1']
+	director_pic = d["FIELD2"]
+
+	print(director_name)
+	print(director_pic)
+
+	f = re.sub('[^A-Za-z0-9]+', '', director_name)
+	
+
+	if director_pic[:4] == 'http':
+		d["FIELD2"] = 'img/directors_pic/' + f + '.jpg'	
+		print(d["FIELD2"])
+		
+
+with open('info_directors.json', 'w') as outfile:
+    json.dump(data_json, outfile)
+
+
+   
+
+data.close()
+outfile.close()
---
+++ example/datas/get_directors_pic.py
@@ -0,0 +1,32 @@
+import os
+import urllib
+import json
+import re
+
+with open('informationDirectors.json') as data_file:    
+	data = json.load(data_file)
+
+newpath = r'directors_pic/'
+
+if not os.path.exists(newpath):    
+		os.makedirs(newpath)
+
+directors = data["directors"][1:]
+
+for d in directors:
+	director_name = d["FIELD1"]
+	director_pic = d["FIELD2"]
+
+ 	print("--- DIRECTOR ---")
+	print(director_name)
+	
+	dpic = d['FIELD2']
+	print(dpic)
+
+	f = re.sub('[^A-Za-z0-9]+', '', director_name)	
+	if 'http' in dpic:
+		urllib.urlretrieve(dpic, newpath + '/' + f + ".jpg")
+		print('--- ok ---')
+
+
+data_file.close()
