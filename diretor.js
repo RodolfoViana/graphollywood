0ce8b91236ccc7c9aef317360a377ec8e75035b1
arthurlustosa <arthur.souza@ccc.ufcg.edu.br>
2016-01-14 14:47:42 -0300

    resolvendo bug

--- diretor.js
+++ diretor.js
@@ -7,7 +7,6 @@ $.getJSON("data/movieDirectors.json", function (data) {
 
     //show a specific director as chart example.
     if (director.length == 0){
-        //location.href="pageNotFound.html";
         director = getDirector("Quentin Tarantino");
     }    
     
@@ -156,9 +155,7 @@ function getDirector(name) { //# groups,# points per group
                     shape: shapes[j % shapes.length]
                 });
             }
-      
-            //location.href="pageNotFound.html";
-        
+        }
     }
     return data;
 }
@@ -217,7 +214,7 @@ function busca(){
                     this.id_director = String(data.directors[i].FIELD6);
                     console.log(id_director);
 
-                    this.biografia = data.directors[i].FIELD3.substring(0,240) + '...' + '<a href="http://www.imdb.com/name/nm'+ this.id_director +'/bio?ref_=nm_ov_bio_sm"> See full bio</a> <br /><br />';
+                    this.biografia = data.directors[i].FIELD3.substring(0,40) + '...' + '<a href="http://www.imdb.com/name/nm'+ this.id_director +'/bio?ref_=nm_ov_bio_sm"> See full bio</a> <br /><br />';
 
                 }
 
