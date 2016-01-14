a670c756b7aab38b0394544e39ec0c2436649309
arthurlustosa <arthur.souza@ccc.ufcg.edu.br>
2016-01-14 14:02:19 -0300

    bug id_director undefined

--- diretor.js
+++ diretor.js
@@ -214,7 +214,7 @@ function busca(){
                     this.id_director = String(data.directors[i].FIELD6);
                     console.log(id_director);
 
-                    this.biografia = data.directors[i].FIELD3.substring(0,140) + '...' + '<a href="http://www.imdb.com/name/nm'+id_director+'/bio?ref_=nm_ov_bio_sm">See full bio</a> <br /><br />';
+                    this.biografia = data.directors[i].FIELD3.substring(0,240) + '...' + '<a href="http://www.imdb.com/name/nm'+ this.id_director +'/bio?ref_=nm_ov_bio_sm"> See full bio</a> <br /><br />';
 
                 }
 
